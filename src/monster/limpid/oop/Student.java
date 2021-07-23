9472d66063b54952f8802773402964174e5ae076
chenzhenghao <1729783561@qq.com>
2021-07-23 21:45:41 +0800

    demo

---
+++ src/monster/limpid/oop/Student.java
@@ -0,0 +1,26 @@
+package monster.limpid.oop;
+
+/**
+ * @author zhenghaochen
+ * @date 2021-06-21 21:24
+ */
+public class Student {
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/monster/limpid/oop/demo.java
@@ -0,0 +1,17 @@
+package monster.limpid.oop;
+
+/**
+ * @author zhenghaochen
+ * @date 2021-06-21 21:24
+ */
+public class demo {
+    public static void main(String[] args) {
+        Student student =  new Student();
+        Student student1 = new Student();
+        Student student2 = new Student();
+    }
+
+    public demo() {
+    }
+    
+}
